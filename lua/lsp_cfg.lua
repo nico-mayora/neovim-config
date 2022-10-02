6e86bb382b14240a0b2abbb4c16ee9159b1d34aa
Nicolás Mayora <nicolas.mayora@fing.edu.uy>
2022-10-02 13:14:50 -0300

    changes

--- lua/lsp_cfg.lua
+++ lua/lsp_cfg.lua
@@ -1,9 +1,3 @@
-vim.diagnostic.config({ virtual_text = false,}) -- Turn off inline diagnostics
-vim.cmd('autocmd CursorHold * lua vim.diagnostic.open_float(0, {scope="cursor"})')
-vim.o.updatetime = 200
-
--- require'lspconfig'.solargraph.setup{}
-require'lspconfig'.hls.setup{}
 require("nvim-lsp-installer").setup({
     automatic_installation = true, -- automatically detect which servers to install (based on which servers are set up via lspconfig)
     ui = {
@@ -14,3 +8,10 @@ require("nvim-lsp-installer").setup({
         }
     }
 })
+
+vim.diagnostic.config({ virtual_text = false,}) -- Turn off inline diagnostics
+vim.cmd('autocmd CursorHold * lua vim.diagnostic.open_float(0, {scope="cursor"})')
+vim.o.updatetime = 200
+
+-- require'lspconfig'.solargraph.setup{}
+require'lspconfig'.hls.setup{}
